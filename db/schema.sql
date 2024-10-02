a635ce4cdbf45aa769821ed9ade553cff3327954
Allison <alligrasson@gmail.com>
2024-10-02 10:45:14 +1000

    Base Pages and Schema

---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE shopapp;
+
+CREATE TABLE collections (
+    id SERIAL PRIMARY KEY,
+    collection_name TEXT,
+    image_url TEXT
+);
+
+CREATE TABLE items (
+    collection_id INTEGER NOT NULL,
+    item_name TEXT,
+    image_url TEXT,
+    description TEXT
+    -- tags ARRAY
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE,
+    password_digest TEXT
+);
+
+INSERT INTO collections (collection_name, image_url)
+VALUES ('hats', 'https://imgs.search.brave.com/tBu0NKX6zt9KoGTNH95kh2s20bEFyt5yhLsJPtLz4mA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzYxNmRpalJURUVM/LmpwZw');
+
+INSERT INTO collections (collection_name, image_url)
+VALUES ('shirts', 'https://imgs.search.brave.com/7Q69jrArOujjYXU9OYBGsxiRbnQ0o54WoTv_GN8bKlA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9paDEu/cmVkYnViYmxlLm5l/dC9pbWFnZS4xODc5/ODU4MjE3LjQwODEv/c3NyY28sc2xpbV9m/aXRfdF9zaGlydCx3/b21lbnMsMTAxMDEw/OjAxYzVjYTI3YzYs/ZnJvbnQsc3F1YXJl/X3Byb2R1Y3QsNjAw/eDYwMC5qcGc');
+
+INSERT INTO collections (collection_name, image_url)
+VALUES ('pants', 'https://imgs.search.brave.com/C4qIdhUICGk8sI5s-_nFRFKPUjrvNJJqVbEcAd5AYas/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YXJvdW5kc3F1YXJl/LmNvbS9jZG4vc2hv/cC9wcm9kdWN0cy9J/TUdfMjI3MF8xY2Iw/NmIxYy1hYTZhLTQ0/OTctYTNkNS1kODlm/YjJiMDJhZGRfZ3Jh/bmRlLmpwZz92PTE2/NTEyNTIxOTI');
+
+INSERT INTO collections (collection_name, image_url)
+VALUES ('shoes', 'https://imgs.search.brave.com/hx6C3dl9AYpqndgzzTeIpGh66R5h327TdaVp-1R-qEA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTF6T3dpWGVXUkwu/anBn');
+
+INSERT INTO collections (collection_name, image_url)
+VALUES ('socks', 'https://imgs.search.brave.com/Y8XqKOgL8b88lRgZ3UCUo-bjqeJ1sx7b1ff_lM_VYLM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zb2Nr/Zmx5LmNvbS9jZG4v/c2hvcC9wcm9kdWN0/cy9Db2xvcmZ1bFNx/dWFyZTFfNzAweDcw/MC5qcGc_dj0xNjMz/NzM0MTc2');
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4567
+const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"));
+// app.use(session({
+//   cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 }, // 3 days
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: true
+// }))
+// app.use(setCurrentUser)
+app.use(expressLayouts) 
+
+
+app.get('/', (req, res) => {    
+    res.render('home')
+})
+
+app.get('/collections', (req, res) => {
+    res.render('collection')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('cart')
+})
+
+app.get('/update', (req, res) => {
+    res.send('updating stock') // take to home page with updated info cause admin
+})
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
