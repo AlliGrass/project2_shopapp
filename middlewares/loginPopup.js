270fe26c5fb08d9fe5b7166a62c164e7942e3452
Allison <alligrasson@gmail.com>
2024-10-03 10:52:59 +1000

    login popup

--- middlewares/loginPopup.js
+++ middlewares/loginPopup.js
@@ -1,16 +1,1 @@
-let loginBtn = document.querySelector(".login-btn");
-    loginBtn.addEventListener("click", openLoginPopup);
-
-function openLoginPopup() {
-    document.querySelector(".login-popup-box").style.display = "block";
-    document.querySelector(".popup-background-overlay").style.display = "block";
-
-    let closeBtn = document.querySelector(".close-popup-btn");
-    closeBtn.addEventListener("click", closeLoginPopup)
-}
-
-function closeLoginPopup() {
-    document.querySelector(".winning-box").style.display = "none";
-    document.querySelector(".winning-background-overlay").style.display = "none";
-}
 
